8e32f63ade9141c0f6f7853688c84cd4e9da75cc
Nicholas Burka <nburka@mac.com>
2014-06-29 04:31:40 -0400

    Theo and more
    hbd

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,5 +1,6 @@
 window.navigator.vibrate([200, 100, 200]);
 var clicked = false;
+var theo = false;
 function show_image(src, target, width, height, alt, className, id) {
 	var img = new Image();
 	img.src = src;
@@ -8,7 +9,7 @@ function show_image(src, target, width, height, alt, className, id) {
 	img.alt = alt;
 	img.className = className;
 	img.setAttribute("id", id);
-	document[target].appendChild(img);
+	document.getElementById(target).appendChild(img);
 	return img;
 	//document.body.insertBefore(img, document.getElementById(target).nextSibling);
 	// document.write("<IMG + SRC=\"" + src + "\" width=" + width + " height=" + height + " alt=\"" + alt + "\">");
@@ -22,9 +23,9 @@ function show_pakdees() {
 	var b = document.getElementById("pakbutton");
 	if (!clicked) {
 	clicked = true;
-	show_image("imgs/pak1.jpg", "body", 100, 100, "A photo of Pakdee", "pakdee", "pakdee1");
-	show_image("imgs/pak2.jpg", "body", 100, 100, "A photo of Pakdee", "pakdee", "pakdee2");
-	show_image("imgs/pak3.jpg", "body", 100, 100, "A photo of Pakdee", "pakdee", "pakdee3");
+	show_image("imgs/pak1.jpg", "pakdeeWrapper", 100, 100, "A photo of Pakdee", "pakdee", "pakdee1");
+	show_image("imgs/pak2.jpg", "pakdeeWrapper", 100, 100, "A photo of Pakdee", "pakdee", "pakdee2");
+	show_image("imgs/pak3.jpg", "pakdeeWrapper", 100, 100, "A photo of Pakdee", "pakdee", "pakdee3");
 	change_hw("pakdee1", 100,100);
 	//b.parentNode.removeChild(b);
 	//it works!
@@ -40,3 +41,89 @@ function show_pakdees() {
 		b.setAttribute("value", "More Pakdee");
 	}
 };
+
+function theotime() {
+	if (theo) {
+		deleteTheoCanvas();
+	} else {makeTheoCanvas();}
+	theo = !theo;
+}
+
+function makeTheoCanvas() {
+	var theoDiv = document.getElementById("theoWrapper");
+	var canvas = document.createElement('canvas');
+	canvas.setAttribute("id", "theoCanvas");
+	canvas.id = "theoCanvas";
+	canvas.width = 600;
+	canvas.height = 600;
+	canvas.style.zIndex="1";
+	//canvas.style.position="absolute";
+	canvas.style.left="0px";
+	canvas.style.top="0px";
+	var desiredHeight = 600;
+	theoDiv.appendChild(canvas);
+	var ctx = canvas.getContext("2d");
+	var img = new Image();
+	img.src = "imgs/theoHead.jpg";
+	img.onload = function() {
+		ctx.drawImage(img, (canvas.width-img.width)/2, (canvas.height-img.height)/2);
+	}
+	canvas.addEventListener('mouseenter', theoMouseOver, false);
+	canvas.addEventListener('mouseleave', theoMouseOut, false);
+	canvas.addEventListener('click', theoClick, false);
+	// var canvas2 = document.createElement('canvas');
+	// canvas2.setAttribute("id", "theoCanvas2");
+	// canvas2.width = 600;
+	// canvas2.height = 600;
+	// canvas2.style.zIndex="2";
+	// canvas2.style.position="absolute";
+	// canvas2.style.left="0px";
+	// canvas2.style.top="0px";
+	// theoDiv.appendChild(canvas2);
+}
+
+function theoMouseOver() {
+	var canvas = document.getElementById("theoCanvas");
+	var ctx = canvas.getContext("2d");
+	//ctx.clearRect(0,0,canvas.width,canvas.height);
+	var img = new Image();
+	img.src = "imgs/theoHeadOpen.png";
+	img.onload = function() {
+		ctx.drawImage(img, (canvas.width-img.width)/2, (canvas.height-img.height)/2);
+	}
+	ctx.font = "20px Arial";
+	ctx.fillStyle = "black";
+	ctx.fillText("fuck", 10, canvas.height-140);
+	ctx.fillText("yooo", 10, canvas.height-110);
+}
+
+function theoMouseOut() {
+	var canvas = document.getElementById("theoCanvas");
+	var ctx = canvas.getContext("2d");
+	//ctx.clearRect(0,0,canvas.width,canvas.height);
+	ctx.clearRect(10,canvas.height-180,100,100);
+	var img = new Image();
+	img.src = "imgs/theoHead.png";
+	img.onload = function() {
+		ctx.drawImage(img, (canvas.width-img.width)/2, (canvas.height-img.height)/2);
+	}
+}
+
+function theoClick(event) {
+	var canvas = document.getElementById("theoCanvas");
+	var x = event.pageX - canvas.offsetLeft;
+	var y = event.pageY - canvas.offsetTop;
+	var ctx = canvas.getContext("2d");
+	var img = new Image();
+	img.src = "imgs/birthdayflowers1.png";
+	var IMGBOUND = 100
+	img.onload = function() {
+		ctx.drawImage(img, x-(IMGBOUND/2), y-(IMGBOUND/2), IMGBOUND, IMGBOUND);
+	}
+}
+
+function deleteTheoCanvas() {
+	var theoDiv = document.getElementById("theoWrapper");
+	theoDiv.removeChild(document.getElementById("theoCanvas"));
+	// theoDiv.removeChild(document.getElementById("theoCanvas2"));
+}
