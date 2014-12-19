a99439904a34ce22a430d97921f1c73d12208e74
Nicholas Burka <nburka@mac.com>
2014-12-19 03:49:09 -0500

    Added resume, made more professional, code cleaning

--- javascripts/interestingenglish.js
+++ javascripts/interestingenglish.js
@@ -1,78 +1,54 @@
 on = true;
 
 var english = function() {
-	/*var api = document.createElement("script");
-	api.type = "text/javascript";
-	api.src = "https://api.stackexchange.com/js/2.0/all.js";
-	document.getElementById("wrapper").appendChild(api);
-*/
+	
 	var id = 3995;
 	var myKey = "M)BVAu6uV98hHLXr4b2i*w((";
 	var OAuthDomain = "http://nicholasburka.github.io";
 	var local = "file:///Users/nburka/Desktop/programming/web/javascript/nicholasburka.github.io/index.html";
 
+	var NUM_ANSWERS = 3;
+
 	SE.init({
 	    clientId: id,
 	    key: myKey,
-	    channelUrl: OAuthDomain,
+	    channelUrl: local,
 	    complete: function (data) {  }
 	});
 
-	function addStuff(data) {
-		//console.log(data);
-		div2.innerHTML = div2.innerHTML + "<ul>";
-		var items = data["items"];
-		var tmp = document.createElement("DIV");
-		var ids = [];
-		var str = "http://api.stackexchange.com/2.2/questions/";
-		for (var i = 0; i < items.length - 1; i ++) {
-			ids[i] = items["post_id"];
-			str += data["items"][i]["question_id"] + ";";
-		}
-		str += data["items"][items.length-1]["question_id"] + "/answers";
-		str += "?site=english&pagesize=10&sort=creation&filter=withbody";
-		//console.log(str);
-		http = new XMLHttpRequest();
-		http.open("GET", str, true);
+	function httpRequest(operation, url, onload, arg) {
+		var http = new XMLHttpRequest();
+		http.open(operation, url, true);
 		http.onload = function() {
-			//console.log("SECOND REQUEST: " + http.responseText);
-			step2(items, JSON.parse(http.responseText));
-		}
+			onload(http.responseText, arg);
+		};
 		http.send();
-
 	}
 
-	function step2(items, response) {
-		//console.log(response);
-		var map = match(items, response["items"]);
+	function cleanAppend(response) {
+		var resp = JSON.parse(response);
+		var items = resp["items"].sort(sorte);
 		var tmp = document.createElement("DIV");
 		var tmp2 = document.createElement("DIV");
-		/*for (var i = 0; i < items.length; i ++) {
+		var len;
+		var sorted;
+		for (var i = 0; i < items.length - 1; i++) {
 			tmp.innerHTML = items[i]["body"];
-			tmp2.innerHTML = response["items"][i]["body"];
-			div2.innerHTML = div2.innerHTML + "<li>" + (i + 1) + ". " + tmp.textContent + "<br><br>" + tmp2.textContent + "</li><p></p><br>";
-		}*/
-		//console.log(map);
-		for (var q in map) {
-
-			/*for (f in q) {
-				//console.log(f);
-			}*/
-			//console.log(q);
-			tmp.innerHTML = map[q]["q"];
-			tmp2.innerHTML = map[q]["a"];
-			if (map[q]["a"]) {
-				//console.log(tmp.innerHTML);
-				div2.innerHTML = div2.innerHTML + "<li>" + "////Q: " + tmp.textContent + "<br><br>A: " + tmp2.textContent + "///" + "</li><br>";
+			div2.innerHTML = div2.innerHTML + "<li>" + "////Q: " + tmp.textContent + "<br><br>";
+			if (items[i]["answer_count"] > 0) {
+				sorted = items[i]["answers"].sort(sorte);
+				if (NUM_ANSWERS < items[i]["answers"].length) {
+					len = NUM_ANSWERS;
+				} else {
+					len = items[i]["answers"].length;
+				}
 			}
-		}/*
-		for (int i = 0; i < map.length; i++) {
-			tmp.innerHTML = map[i].q;
-			tmp2.innerHTML = map[i].a;
-			//console.log(tmp.innerHTML);
-			div2.innerHTML = div2.innerHTML + "<li>" + tmp.textContent + "<br><br>" + tmp2.textContent + "</li><br>";
-		}*/
-		div2.innerHTML = div2.innerHTML + "</ul>"
+			for (var j = 0; j < len; j++) {
+				tmp2.innerHTML = sorted[j]["body"];
+				div2.innerHTML = div2.innerHTML + "A: " + tmp2.textContent + "<br><br>";
+			}
+			div2.innerHTML = div2.innerHTML + "</li><br>";
+		}
 	}
 
 	function match(questions, answers) {
@@ -84,60 +60,23 @@ var english = function() {
 		for (var j = 0; j < answers.length; j++) {
 			map["a" + answers[j]["question_id"]]["a"] = answers[j]["body"];
 		}
-		//console.log(map);
 		return map;
 	}
 
-	function convertHtmlToText(input) {
-    var inputText = input
-    var returnText = "" + inputText;
+	//sorts array of objs by field
+	function sorte(a, b) {
+		return (a["score"] < b["score"]);
+	}
 
-    //-- remove BR tags and replace them with line break
-    returnText=returnText.replace(/<br>/gi, "\n");
-    returnText=returnText.replace(/<br\s\/>/gi, "\n");
-    returnText=returnText.replace(/<br\/>/gi, "\n");
-
-    //-- remove P and A tags but preserve what's inside of them
-    returnText=returnText.replace(/<p.*>/gi, "\n");
-    returnText=returnText.replace(/<a.*href="(.*?)".*>(.*?)<\/a>/gi, " $2 ($1)");
-
-    //-- remove all inside SCRIPT and STYLE tags
-    returnText=returnText.replace(/<script.*>[\w\W]{1,}(.*?)[\w\W]{1,}<\/script>/gi, "");
-    returnText=returnText.replace(/<style.*>[\w\W]{1,}(.*?)[\w\W]{1,}<\/style>/gi, "");
-    //-- remove all else
-    returnText=returnText.replace(/<(?:.|\s)*?>/g, "");
-
-    //-- get rid of more than 2 multiple line breaks:
-    returnText=returnText.replace(/(?:(?:\r\n|\r|\n)\s*){2,}/gim, "\n\n");
-
-    //-- get rid of more than 2 spaces:
-    returnText = returnText.replace(/ +(?= )/g,'');
-
-    //-- get rid of html-encoded characters:
-    returnText=returnText.replace(/&nbsp;/gi," ");
-    returnText=returnText.replace(/&amp;/gi,"&");
-    returnText=returnText.replace(/&quot;/gi,'"');
-    returnText=returnText.replace(/&lt;/gi,'<');
-    returnText=returnText.replace(/&gt;/gi,'>');
-
-    //-- return
-    return returnText;
-}
-
-	var xmlHttp = null;
 	var div2 = document.getElementById("interestingEnglish");
+	var button = document.getElementById("englishButton");
 	if (on) {
-		document.getElementById("interestingEnglish").style = "visibility = visible";
+		httpRequest("GET", "http://api.stackexchange.com/2.2/questions?sort=activity&order=desc&pagesize=10&site=english&filter=!b0OfNJc4Ohv7uP", cleanAppend, undefined);
+		button.value="Less discussion";
 	} else {
-		document.getElementById("interestingEnglish").style = "visibility = hidden";
+		div2.innerHTML = "";
+		button.value="More English language discussion";
 	}
 	on = !on;
-	xmlHttp = new XMLHttpRequest();
-	xmlHttp.open( "GET", "http://api.stackexchange.com/2.2/questions?sort=votes&order=desc&pagesize=10&max=15&site=english&filter=withbody", true );
-	xmlHttp.responseType = ""
-	xmlHttp.onload = function () {
-		addStuff(JSON.parse(xmlHttp.responseText));
-	}
-	xmlHttp.send();
 
 }
--- javascripts/script.js
+++ javascripts/script.js
@@ -1,6 +1,11 @@
+//Well hello, mobile broswers :)
 window.navigator.vibrate([200, 100, 200]);
-var clicked = false;
+
+
+var pakdee = false;
 var theo = false;
+var resumeThere = false;
+
 function show_image(src, target, width, height, alt, className, id) {
 	var img = new Image();
 	img.src = src;
@@ -11,9 +16,21 @@ function show_image(src, target, width, height, alt, className, id) {
 	img.setAttribute("id", id);
 	document.getElementById(target).appendChild(img);
 	return img;
-	//document.body.insertBefore(img, document.getElementById(target).nextSibling);
-	// document.write("<IMG + SRC=\"" + src + "\" width=" + width + " height=" + height + " alt=\"" + alt + "\">");
 }
+
+function resume() {/*
+	var resId = "resumeImg";
+	if (!resumeThere) {
+		show_image("imgs/resume.pdf", "resume", 100, 100, "My sparkly clean resume, for your appraisal", "Here is where my resume will appear", resId);
+	}
+	else {
+		var res = document.getElementById(resId);
+		res.parentNode.removeChild(res);
+	}
+	resumeThere = !resumeThere;*/
+	window.open("imgs/resume.pdf","_blank");
+}
+
 function change_hw(id, h, w) {
 	document.getElementById(id).style.height = h;
 	document.getElementById(id).style.width = w;
@@ -22,17 +39,13 @@ function change_hw(id, h, w) {
 function show_pakdees() {
 	var b = document.getElementById("pakbutton");
 	var IMG_SIZE = 100;
-	if (!clicked) {
-	clicked = true;
+	if (!pakdee) {
 	show_image("imgs/pak1.jpg", "pakdeeWrapper", IMG_SIZE, IMG_SIZE, "A photo of Pakdee", "pakdee", "pakdee1");
 	show_image("imgs/pak2.jpg", "pakdeeWrapper", IMG_SIZE, IMG_SIZE, "A photo of Pakdee", "pakdee", "pakdee2");
 	show_image("imgs/pak3.jpg", "pakdeeWrapper", IMG_SIZE, IMG_SIZE, "A photo of Pakdee", "pakdee", "pakdee3");
 	change_hw("pakdee1", 100,100);
-	//b.parentNode.removeChild(b);
-	//it works!
 	b.setAttribute('value', "Less Pakdee");
 	} else {
-		clicked = false;
 		var one = document.getElementById("pakdee1");
 		var two = document.getElementById("pakdee2");
 		var three = document.getElementById("pakdee3");
@@ -41,6 +54,7 @@ function show_pakdees() {
 		three.parentNode.removeChild(three);
 		b.setAttribute("value", "More Pakdee");
 	}
+	pakdee = !pakdee;
 };
 
 function theotime() {
@@ -53,34 +67,28 @@ function theotime() {
 function makeTheoCanvas() {
 	var theoDiv = document.getElementById("theoWrapper");
 	var canvas = document.createElement('canvas');
+
 	canvas.setAttribute("id", "theoCanvas");
 	canvas.id = "theoCanvas";
 	canvas.width = 600;
 	canvas.height = 600;
 	canvas.style.zIndex="1";
-	//canvas.style.position="absolute";
 	canvas.style.left="0px";
 	canvas.style.top="0px";
+
 	var desiredHeight = 600;
 	theoDiv.appendChild(canvas);
 	var ctx = canvas.getContext("2d");
 	var img = new Image();
 	img.src = "imgs/theoHead.jpg";
+
 	img.onload = function() {
 		ctx.drawImage(img, (canvas.width-img.width)/2, (canvas.height-img.height)/2);
 	}
+
 	canvas.addEventListener('mouseenter', theoMouseOver, false);
 	canvas.addEventListener('mouseleave', theoMouseOut, false);
 	canvas.addEventListener('click', theoClick, false);
-	// var canvas2 = document.createElement('canvas');
-	// canvas2.setAttribute("id", "theoCanvas2");
-	// canvas2.width = 600;
-	// canvas2.height = 600;
-	// canvas2.style.zIndex="2";
-	// canvas2.style.position="absolute";
-	// canvas2.style.left="0px";
-	// canvas2.style.top="0px";
-	// theoDiv.appendChild(canvas2);
 }
 
 function theoMouseOver() {
@@ -94,8 +102,8 @@ function theoMouseOver() {
 	}
 	ctx.font = "20px Arial";
 	ctx.fillStyle = "black";
-	ctx.fillText("fuck", 10, canvas.height-140);
-	ctx.fillText("yooo", 10, canvas.height-110);
+	ctx.fillText("click", 10, canvas.height-140);
+	ctx.fillText("me", 10, canvas.height-110);
 }
 
 function theoMouseOut() {
@@ -108,10 +116,7 @@ function theoMouseOut() {
 	img.onload = function() {
 		ctx.drawImage(img, (canvas.width-img.width)/2, (canvas.height-img.height)/2);
 	}
-	ctx.font = "20px Arial";
-	ctx.fillStyle = "white";
-	ctx.fillText("fuck", 10, canvas.height-140);
-	ctx.fillText("yooo", 10, canvas.height-110);
+	ctx.clearRect(0, canvas.height-200, 60, 120);
 }
 
 function theoClick(event) {
